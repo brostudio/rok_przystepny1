a5de04c08c6cd8966f3edc3b54b6a08f4b7e356a
Student <marcin@brostudio.pl>
2022-11-18 15:45:41 +0100

    With Jenkinsfile

--- rok_przystepny.py
+++ rok_przystepny.py
@@ -1,26 +1,24 @@
 """
 Program sprawdza czy podany rok jest przestępny
 """
-def sprawdz_rok(rok):
+def rok_przystepny(year_of_test):
+    # wprowadzenie danych wejściowych
     # obliczenia
     # wyrażenie zwraca wartość logiczną
-    czy_przestepny = (rok % 400 == 0) or (rok % 100 != 0) and (rok % 4 == 0)
+    czy_przestepny = (year_of_test % 400 == 0) or (year_of_test % 100 != 0) and (year_of_test % 4 == 0)
 
     # wypisanie wyniku
-    print('Czy rok', rok, 'jest przestępny?', czy_przestepny)
+    print('Czy rok', year_of_test, 'jest przestępny?', czy_przestepny)
 
     return czy_przestepny
 
 
 if __name__ == '__main__':
-    # wprowadzenie danych wejściowych
-    rok = int(input('Podaj rok: '))
-    sprawdz_rok(rok)
+    rok = int(input("Podaj rok"))
+    rok_przystepny(rok)
 
+def test_rok_ma_byc_przystepny():
+    assert rok_przystepny(2020) == True
 
-def test_czy_rok_przystepny():
-    assert sprawdz_rok(2020) == True
-
-
-def test_czy_rok_nie_przystepny():
-    assert sprawdz_rok(2018) == False
+def test_rok_ma_byc_nieprzystepny():
+    assert rok_przystepny(2022) == False
